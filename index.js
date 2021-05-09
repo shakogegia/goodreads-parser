b8e6b2d63fde773164e40d7534331c0dcc25b17f
shakogegia <shalva.gegia@gmail.com>
2021-05-09 15:52:20 +0200

    fix: return goodreads id

--- index.js
+++ index.js
@@ -3,13 +3,36 @@ const jsdom = require("jsdom");
 const { parseBookPage } = require("./utils");
 const { JSDOM } = jsdom;
 
+const getResult = (result, response) => {
+  const url = response.request.res.responseUrl
+  let id = ''
+  
+  let urlParts = url.split('book/show/')
+  if (urlParts.length > 1) {
+    urlParts = urlParts[1]
+    if (urlParts.split('.').length > 1) {
+      id = urlParts.split('.')[0]
+    } else if (urlParts.split('-').length > 1) {
+      id = urlParts.split('-')[0]
+    } else {
+      id = parseInt(urlParts)
+    }
+  }
+
+  return ({ ...result, id, url})
+}
+
 module.exports.parseByISBN13 = async isbn13 => {
   try {
     const response = await axios.get("https://www.goodreads.com/search?q="+isbn13);
+    const { data, ...da } = response
+    
+
+    
     const dom = new JSDOM(response.data);
     const { document } = dom.window;
     const result = parseBookPage(document);
-    return result;
+    return getResult(result, response)
   } catch (error) {
     throw error;
   }
@@ -21,7 +44,7 @@ module.exports.parseByURL = async url => {
     const dom = new JSDOM(response.data);
     const { document } = dom.window;
     const result = parseBookPage(document);
-    return result;
+    return getResult(result, response)
   } catch (error) {
     throw error;
   }
